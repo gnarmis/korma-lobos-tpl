ff45fcf5d87855eb46b64fad85bcd43ac67a5b11
Gursimran Singh <g13singh@gmail.com>
2012-07-04 17:27:15 -0500

    added a sample secret file

---
+++ src/korma_lobos_tpl/secret-sample.clj
@@ -0,0 +1,8 @@
+(ns korma-lobos-tpl.secret-sample)
+
+(def ^:dynamic *db-name* "")
+(def ^:dynamic *db-user* "")
+(def ^:dynamic *db-password* "")
+(def ^:dynamic *db-host* "127.0.0.1")
+(def ^:dynamic *db-port* 3306)
+
