7b111d697f73d700e1abf83dfbab0e8bb59424d0
Gursimran Singh <g13singh@gmail.com>
2012-07-03 22:36:28 -0500

    cleanup

--- src/korma_lobos_tpl/models/dbspec.clj
+++
@@ -1,9 +0,0 @@
-(ns korma-lobos-tpl.models.dbspec
-	(:require [clojure.string :as string])
-	(:use [korma.db]))
-
-; development database
-(defdb dev
-	(mysql {:db *db-name*
-			:user *db-user*
-			:password *db-password*}))
